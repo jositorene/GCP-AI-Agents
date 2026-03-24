dccb01632ae4ca8e5442fcd68930e2ffba9b9ba1
jositorene <jrfuser01@gmail.com>
2026-03-24 16:28:17 +0100

    Delete Interfase/app directory
    App directory delete to update the new code

--- Interfase/app/js/scripts.js
+++
@@ -1,6 +0,0 @@
-console.log("Login page loaded");
-
-// Example: future enhancements
-document.addEventListener("DOMContentLoaded", function () {
-    console.log("DOM ready");
-});
--- Interfase/app/text/messages.py
+++
@@ -1,5 +0,0 @@
-# simple text info
-LOGIN_INFO = """
-Welcome! Please login with your username and password.
-Choose your user type: Subscriber or Journalist.
-"""
